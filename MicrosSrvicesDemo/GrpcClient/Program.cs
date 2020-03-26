75ae7f203975bc21f99a0b69c3743a998b84a44d
shuiwazi <haws001@DESKTOP-PQR>
2020-03-26 16:15:54 +0800

    增加grpc

---
+++ MicrosSrvicesDemo/GrpcClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///单词调用
+            ////1、开始调用连接
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //// 2、创建客户端
+            //var client = new GrpcService.GrpcServiceClient(channel);
+            ////开始请求
+            //var reply = client.Student(new RmRequest { Name = "小明" });
+
+            //Console.WriteLine(reply.Message);
+            //Console.ReadKey();
+
+
+            ///流式调用
+            Grpc();
+
+            Console.ReadKey();
+        }
+        private static async void Grpc()
+        {
+            //1、开始调用连接
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            // 2、创建客户端
+            var client = new GrpcService.GrpcServiceClient(channel);
+
+            #region 服务器流式处理 (服务器可以响应多个函数)
+            var responsesStream = client.StudentServer(
+                 new RmRequest { Name = "小强" }
+            );
+
+            var readStream = responsesStream.ResponseStream;
+
+            // 1、获取一次输出（for 循环每一次写出）
+            while (await readStream.MoveNext())
+            {
+                RmResponse rmResponse = readStream.Current;
+
+                Console.WriteLine($"客户端收到请求:{rmResponse.Message}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+
+            #endregion
+        }
+    }
+}
---
+++ MicrosSrvicesDemo/GrpcServer/Services/GrpcDemo.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class GrpcDemo: GrpcService.GrpcServiceBase
+    {
+          public override Task<RmResponse> Student(RmRequest request, ServerCallContext context)
+        {
+            // 1、打印客户端信息
+            Console.WriteLine($"request学生姓名:{request.Name}");
+            // 2、返回
+            return Task.FromResult(new RmResponse
+            {
+                Message=$"{request.Name}请求了一次服务器，已经收到"
+            }
+                );
+        }
+        public override async Task StudentServer(RmRequest request, IServerStreamWriter<RmResponse> responseStream, ServerCallContext context)
+        {
+
+            // 1、打印客户端信息
+            Console.WriteLine($"request学生姓名:{request.Name}");
+            // 2、流式处理(函数流)
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(1000);
+                RmResponse rmResponse = new RmResponse()
+                {
+                    Message = "函数流处理，结果：" + i
+                };
+                await responseStream.WriteAsync(rmResponse);
+            }
+        }
+
+        }
+}
